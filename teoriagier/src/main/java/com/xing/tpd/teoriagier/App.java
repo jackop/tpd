effafb76754ff6ba38752ec8bf555b12d7eb3cf4
Pawel Ciazynski <pawel.ciazynski@ericsson.com>
2019-04-10 20:53:32 +0200

    Added teoriagier project

---
+++ teoriagier/src/main/java/com/xing/tpd/teoriagier/App.java
@@ -0,0 +1,13 @@
+package com.xing.tpd.teoriagier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
